e3aa07a5df4df236af2b4754a401eb0af607c7bd
zare926 <ilove.boc926@gmail.com>
2020-07-29 00:05:39 +0900

    Todoが入力できるようにする

--- js/main.js
+++ js/main.js
@@ -3,11 +3,18 @@
   var vm = new Vue({
     el: '#app',
     data: {
+      newItem:'',
       todos:[
         'task1',
         'task2',
         'task3'
       ]
+    },
+    methods: {
+      addItem: function(e){
+        e.preventDefault();
+        this.todos.push(this.newItem);
+      }
     }
   });
   // two way data binding (to UI)
