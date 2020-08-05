1da19ca8873985dbe66b7cd02c96e5bad9949086
zare926 <ilove.boc926@gmail.com>
2020-08-06 06:54:14 +0900

    初期値設定

--- js/main.js
+++ js/main.js
@@ -4,16 +4,7 @@
     el: '#app',
     data: {
       newItem:'',
-      todos:[{
-        title: 'task1',
-        isDone: false
-      },{
-        title: 'task2',
-        isDone: false
-      },{
-        title: 'task3',
-        isDone: true
-      }]
+      todos: []
     },
     methods: {
       addItem: function(){
