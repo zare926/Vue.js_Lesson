3c04c307950916a693604398d75c5a815c46ca31
zare926 <ilove.boc926@gmail.com>
2020-08-06 07:15:18 +0900

    一括削除

--- js/main.js
+++ js/main.js
@@ -19,14 +19,19 @@
         if (confirm('are you sure?')){
           this.todos.splice(index, 1);
         }
+      },
+      purge: function(){
+        if (!confirm('delete finished?')){
+          return;
+        }
+        this.todos = this.remaining;
       }
     },
     computed: {
       remaining: function(){
-        var items = this.todos.filter(function(todo){
+        return this.todos.filter(function(todo){
           return !todo.isDone;
         });
-        return items.length;
       }
     }
   });
