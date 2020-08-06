699e663ba8fde4b02b7af791a0b0e5370b492865
zare926 <ilove.boc926@gmail.com>
2020-08-06 21:11:28 +0900

    ローカルストレージ

--- js/main.js
+++ js/main.js
@@ -6,6 +6,22 @@
       newItem:'',
       todos: []
     },
+    watch: {
+      // todos: function() {
+        // localStorage.setItem('todos',JSON.stringify(this.todos));
+        // alert('Data saved!');
+      // }
+      todos: {
+        handler: function() {
+          localStorage.setItem('todos',JSON.stringify(this.todos));
+          // alert('Data saved!');
+        },
+        deep: true
+      }
+    },
+    mounted: function(){
+      this.todos = JSON.parse(localStorage.getItem('todos')) || [];
+    },
     methods: {
       addItem: function(){
         var item = {
